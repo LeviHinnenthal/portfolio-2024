8f3277e1a8073923215b6aa53c31c1198a0a26a8
LeviHinnenthal <106553886+LeviHinnenthal@users.noreply.github.com>
2024-08-05 09:23:40 +0200

    myprojects

--- components/MyProjects.tsx
+++ components/MyProjects.tsx
@@ -1,102 +1,65 @@
 "use client";
 import React from "react";
 import { HeroParallax } from "./ui/HeroParallax";
+import DaunkuPng from "../public/Daunku.png"
+import CarRentalPng from "../public/CarRental.png"
+import NikeStorePng from "../public/nikeStore.png"
+
 
 export function MyProjects() {
-  return <HeroParallax  products={products} />;
+  return <HeroParallax products={products} />;
 }
 export const products = [
   {
-    title: "Moonbeam",
-    link: "https://gomoonbeam.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/moonbeam.png",
+    title: "Car Rental",
+    link: "https://car-next-silk.vercel.app/",
+    thumbnail: CarRentalPng,
   },
   {
-    title: "Cursor",
-    link: "https://cursor.so",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/cursor.png",
+    title: "Nike Store",
+    link: "https://nike-store-dusky.vercel.app/",
+    thumbnail: NikeStorePng,
   },
   {
-    title: "Rogue",
-    link: "https://userogue.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/rogue.png",
+    title: "Daunku",
+    link: "https://interior-plants.vercel.app/",
+    thumbnail: DaunkuPng,
   },
 
   {
-    title: "Editorially",
-    link: "https://editorially.org",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/editorially.png",
+    title: "Car Rental",
+    link: "https://car-next-silk.vercel.app/",
+    thumbnail: CarRentalPng,
   },
   {
-    title: "Editrix AI",
-    link: "https://editrix.ai",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/editrix.png",
+    title: "Nike Store",
+    link: "https://nike-store-dusky.vercel.app/",
+    thumbnail: NikeStorePng,
   },
   {
-    title: "Pixel Perfect",
-    link: "https://app.pixelperfect.quest",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
+    title: "Daunku",
+    link: "https://interior-plants.vercel.app/",
+    thumbnail: DaunkuPng,
   },
 
-  {
-    title: "Algochurn",
-    link: "https://algochurn.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
-  },
-  {
-    title: "Aceternity UI",
-    link: "https://ui.aceternity.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
-  },
-  {
-    title: "Tailwind Master Kit",
-    link: "https://tailwindmasterkit.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
-  },
-  {
-    title: "SmartBridge",
-    link: "https://smartbridgetech.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
-  },
-  {
-    title: "Renderwork Studio",
-    link: "https://renderwork.studio",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
-  },
 
   {
-    title: "Creme Digital",
-    link: "https://cremedigital.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
+    title: "Car Rental",
+    link: "https://car-next-silk.vercel.app/",
+    thumbnail: CarRentalPng,
   },
   {
-    title: "Golden Bells Academy",
-    link: "https://goldenbellsacademy.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
+    title: "Nike Store",
+    link: "https://nike-store-dusky.vercel.app/",
+    thumbnail: NikeStorePng,
   },
   {
-    title: "Invoker Labs",
-    link: "https://invoker.lol",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/invoker.png",
+    title: "Daunku",
+    link: "https://interior-plants.vercel.app/",
+    thumbnail: DaunkuPng,
   },
-  {
-    title: "E Free Invoice",
-    link: "https://efreeinvoice.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
-  },
+
+
+
+
 ];
