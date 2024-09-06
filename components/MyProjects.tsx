21bff0d9511f7468edc2bb2e83300d4c70e16c45
LeviHinnenthal <106553886+LeviHinnenthal@users.noreply.github.com>
2024-09-06 14:20:59 +0200

    chane images

--- components/MyProjects.tsx
+++ components/MyProjects.tsx
@@ -4,7 +4,12 @@ import { HeroParallax } from "./ui/HeroParallax";
 import DaunkuPng from "../public/Daunku.png"
 import CarRentalPng from "../public/CarRental.png"
 import NikeStorePng from "../public/nikeStore.png"
-
+import App1 from "../public/app1.png"
+import App2 from "../public/app2.png"
+import App3 from "../public/app3.png"
+import App4 from "../public/app4.png"
+import App5 from "../public/app5.png"
+import App6 from "../public/app6.png"
 
 export function MyProjects() {
   return <HeroParallax products={products} />;
@@ -61,37 +66,37 @@ export const products = [
 
 
   {
-    title: "Car Rental",
-    link: "https://car-next-silk.vercel.app/",
-    thumbnail: CarRentalPng,
-  },
-  {
-    title: "Nike Store",
-    link: "https://nike-store-dusky.vercel.app/",
-    thumbnail: NikeStorePng,
-  },
-  {
-    title: "Daunku",
-    link: "https://interior-plants.vercel.app/",
-    thumbnail: DaunkuPng,
+    title: "Uber app",
+    link: "/",
+    thumbnail: App1,
   },
 
 
+  {
+    title: "Uber app",
+    link: "/",
+    thumbnail: App2,
+  },
+  {
+    title: "Uber app",
+    link: "/",
+    thumbnail: App3,
+  },
 
   {
-    title: "Car Rental",
-    link: "https://car-next-silk.vercel.app/",
-    thumbnail: CarRentalPng,
+    title: "Uber app",
+    link: "/",
+    thumbnail: App4,
   },
   {
-    title: "Nike Store",
-    link: "https://nike-store-dusky.vercel.app/",
-    thumbnail: NikeStorePng,
+    title: "Uber app",
+    link: "/",
+    thumbnail: App5,
   },
   {
-    title: "Daunku",
-    link: "https://interior-plants.vercel.app/",
-    thumbnail: DaunkuPng,
+    title: "Uber app",
+    link: "/",
+    thumbnail: App6,
   },
 
 
